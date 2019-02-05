8e4739d297dcefd4d6d7cd617c4a2c9c90e14158
Mel <pro_melos@hotmail.com>
2019-02-05 18:10:45 +0100

    primer commit limpiando proyecto

--- proyectoenv/Scripts/django-admin.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-#!c:\users\pro_m\proyectos\proyectoenv\scripts\python.exe
-=======
-#!C:\Users\Mel\proyectos\proyectoenv\Scripts\python.exe
->>>>>>> gitmel/master
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
