8ef1a1b806c5e606debdf9e25d9145224fe0fbb4
Mel <pro_melos@hotmail.com>
2019-02-06 18:13:45 +0100

    librerias de gráficas funcionand

--- base_pruebas/views.py
+++ base_pruebas/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def base_prueba(request):
-    return render(request, 'base_pruebas/base_prueba.html', {})
+    context  = {
+        'Pepe': '100',
+        'adios': 'que tal'
+    }
+    return render(request, 'base_pruebas/base_prueba.html', context)
